b4c8ad8751bf8ae9783c76d4c2cc6da38f4754ef
Amulya-Jadi <annujadi@gmail.com>
2024-02-29 09:45:48 +0530

    Array Methods - Fruits

---
+++ fruits.js
@@ -0,0 +1,18 @@
+let fruits = []
+fruits[0]='mango'
+fruits.unshift('kiwi')
+fruits.unshift('cherry')
+fruits.push('papaya')
+fruits.push('pineapple')
+fruits.push('banana')
+console.log(fruits)
+
+
+fruits.shift()
+console.log(fruits)
+fruits.shift()
+console.log(fruits)
+fruits.pop()
+console.log(fruits)
+fruits.pop()
+console.log(fruits)
